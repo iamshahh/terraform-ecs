192c9a1889753afa5624be9e858c3936345a978d
ganez-bhoosal <52504583+ganez-bhoosal@users.noreply.github.com>
2020-02-26 20:22:19 -0500

    Update launch-config.tf

--- launch-config.tf
+++ launch-config.tf
@@ -6,7 +6,7 @@ resource "aws_launch_configuration" "default" {
 
     root_block_device {
       volume_type = "standard"
-      volume_size = 20
+      volume_size = 30
       delete_on_termination = true
     }
 
